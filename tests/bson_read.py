f409bc1648b47751f2fda272bc0b08b67da4c16d
iedc <lijunli2598@gmail.com>
2013-10-16 22:11:17 +0800

    add ip

--- tests/bson_read.py
+++ tests/bson_read.py
@@ -5,14 +5,47 @@ import csv
 import json
 import sys
 import re
+import urllib, urllib2
 sys.path.append('../xapian_weibo')
 from datetime import datetime, date
 from bs_input import KeyValueBSONInput
 from xml.dom import minidom
 from xml.etree import ElementTree
-from xml.etree.ElementTree import ElementTree, Element, SubElement, dump, Comment, tostring  
+from xml.etree.ElementTree import ElementTree, Element, SubElement, dump, Comment, tostring
+from random import randrange
 
 BSON_FILEPATH = "/opt/backup/mongodump/20130129/master_timeline/master_timeline_weibo.bson"
+
+def generateIP():
+##    rand_ip = [58,59,60,61,121,122,123,124,125,202,210,218,219,222]
+##    indexOne = randrange(0, 14, 1)
+##    blockOne = rand_ip[indexOne]
+    blockOne = 202
+    blockTwo = randrange(96, 112, 1)
+    blockThree = randrange(0, 255, 1)
+    blockFour = 0
+    if blockOne == 10:
+	return self.__generateRandomIP__()
+    elif blockOne == 172:
+	return self.__generateRandomIP__()
+    elif blockOne == 192:
+	return self.__generateRandomIP__()
+    else:
+	return str(blockOne) + '.' + str(blockTwo) + '.' + str(blockThree) + '.' + str(blockFour)
+
+def IP_City(ip):
+    url = 'http://api.map.baidu.com/location/ip?ak=E432f09c6597f75cc3cf55a71a685ae1&ip=%s&coor=bd09ll' % ip
+    req = urllib2.Request(url)
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    try:
+        res = json.loads(res)
+    except ValueError:
+        return 'Error'
+    if 'content' not in res:
+        return 'Error'
+    else:
+        return res['content']['address']
 
 def load_bs(bs_filepath=BSON_FILEPATH):
     print 'bson file mode: 从备份的BSON文件中 %s 加载数据' % (bs_filepath)
@@ -95,7 +128,7 @@ def load_weibo_from_bs():
         
         h = h + 1
         print h
-        if h > 100:
+        if h > 4:
             break
         if not weibo['retweeted_status']:
             xml_data = ElementTree()
@@ -111,6 +144,16 @@ def load_weibo_from_bs():
             root = buildET('attitudes_count',root,attitudes_count)
             root = buildET('geo',root,geo)
 
+            weibo_ip = generateIP()#随机产生一个微博的IP地址
+            weibo_city = IP_City(weibo_ip)#将随机产生的IP解析成对应的城市
+
+            while weibo_city == 'Error':#判断IP是否存在
+                weibo_ip = generateIP()
+                weibo_city = IP_City(weibo_ip)
+
+            root = buildET('IP',root,weibo_ip)            
+            root = buildET('address',root,weibo_city)
+            
             root = subET(user,'user',root)
 
             xml_data.write(f, 'utf-8')
@@ -136,10 +179,31 @@ def load_weibo_from_bs():
             root = buildET('attitudes_count',root,attitudes_count)
             root = buildET('geo',root,geo)
 
+            weibo_ip = generateIP()#随机产生一个微博的IP地址
+            weibo_city = IP_City(weibo_ip)#将随机产生的IP解析成对应的城市
+
+            while weibo_city == 'Error':#判断IP是否存在
+                weibo_ip = generateIP()
+                weibo_city = IP_City(weibo_ip)
+
+            root = buildET('IP',root,weibo_ip)
+            root = buildET('address',root,weibo_city)
+
             root = subET(user,'user',root)
 
             retweeted_data = Element('retweeted_status')
             root.append(retweeted_data)
+
+##            retweeted_ip = generateIP()#随机产生一个微博的IP地址
+##            retweeted__city = IP_City(retweeted__ip)#将随机产生的IP解析成对应的城市
+##
+##            while retweeted__city == 'Error':#判断IP是否存在
+##                retweeted__ip = generateIP()
+##                retweeted__city = IP_City(retweeted__ip)
+
+            SubElement(retweeted_data, 'r_ip').text = weibo_ip
+            SubElement(retweeted_data, 'r_address').text = weibo_city
+
             SubElement(retweeted_data, 'r_id').text = str(weibo['retweeted_status']['mid'])
             SubElement(retweeted_data, 'r_created').text = weibo['retweeted_status']['created_at']
             SubElement(retweeted_data, 'r_text').text = weibo['retweeted_status']['text']
